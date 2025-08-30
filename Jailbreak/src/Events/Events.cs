a5aa53025e3bfe6b1c1d916970e1fe348588c12a
T3Marius <168598779+T3Marius@users.noreply.github.com>
2025-08-30 16:56:16 +0300

    feat: added events and listeners handlers.

---
+++ Jailbreak/src/Events/Events.cs
@@ -0,0 +1,73 @@
+using static CounterStrikeSharp.API.Core.Listeners;
+using CounterStrikeSharp.API.Core;
+using static Jailbreak.Jailbreak;
+using CounterStrikeSharp.API;
+
+namespace Jailbreak;
+
+public static class Events
+{
+    public static void RegisterEventsHandlers()
+    {
+        Instance.RegisterEventHandler<EventPlayerDisconnect>(OnPlayerDisconnect);
+        Instance.RegisterEventHandler<EventPlayerConnectFull>(OnPlayerConnectFull);
+        Instance.RegisterEventHandler<EventPlayerTeam>(OnPlayerTeam);
+    }
+    public static void RegisterListeners()
+    {
+        Instance.RegisterListener<OnTick>(OnTick);
+    }
+    private static HookResult OnPlayerTeam(EventPlayerTeam @event, GameEventInfo info)
+    {
+        CCSPlayerController? player = @event.Userid;
+        if (player == null)
+            return HookResult.Continue;
+
+        Server.NextFrame(() =>
+        {
+            var jbPlayer = JBPlayerManagement.GetOrCreate(player);
+            jbPlayer.UpdateRole();
+        });
+
+        return HookResult.Continue;
+    }
+    private static HookResult OnPlayerConnectFull(EventPlayerConnectFull @event, GameEventInfo info)
+    {
+        CCSPlayerController? userId = @event.Userid;
+        if (userId == null)
+            return HookResult.Continue;
+
+        JBPlayerManagement.GetOrCreate(userId);
+
+        return HookResult.Continue;
+    }
+    private static HookResult OnPlayerDisconnect(EventPlayerDisconnect @event, GameEventInfo info)
+    {
+        CCSPlayerController? userId = @event.Userid;
+        if (userId == null)
+            return HookResult.Continue;
+
+        var player = JBPlayerManagement.GetOrCreate(userId);
+
+        player.OnDisconnect();
+        JBPlayerManagement.Remove(userId.SteamID);
+
+        return HookResult.Continue;
+    }
+    private static void OnTick()
+    {
+        foreach (var userId in Utilities.GetPlayers())
+        {
+            var player = JBPlayerManagement.GetOrCreate(userId);
+
+            if (player.HtmlMessages.TryGetValue(userId, out string? message))
+            {
+                userId.PrintToCenterHtml(message);
+            }
+            else
+            {
+                // do nothing
+            }
+        }
+    }
+}
