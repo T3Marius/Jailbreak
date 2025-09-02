5e447f8008bc9877bb5fee64de19d67a7aa7f01c
T3Marius <168598779+T3Marius@users.noreply.github.com>
2025-09-02 05:37:09 +0300

    feat: added special days system

---
+++ Jailbreak/src/Menu/SpecialDays.cs
@@ -0,0 +1,34 @@
+using CounterStrikeSharp.API.Core.Translations;
+using static Jailbreak.Jailbreak;
+
+namespace Jailbreak;
+
+public static class SpecialDaysMenu
+{
+    public static void Display(JBPlayer jbPlayer, IT3Menu? parentMenu = null)
+    {
+        IT3Menu menu = MenuManager.CreateMenu(Instance.Localizer.ForPlayer(jbPlayer.Controller, "special_days_menu<title>"));
+        menu.FreezePlayer = false;
+
+        if (parentMenu != null)
+        {
+            menu.IsSubMenu = true;
+            menu.ParentMenu = parentMenu;
+        }
+
+        IReadOnlyList<ISpecialDay> Days = SpecialDayManagement.GetDays();
+
+        foreach (var day in Days)
+        {
+            menu.AddOption(day.Name, (p, o) =>
+            {
+                SpecialDayManagement.SelectDay(jbPlayer, day.Name);
+            });
+        }
+
+        if (parentMenu == null)
+            MenuManager.OpenMainMenu(jbPlayer.Controller, menu);
+        else
+            MenuManager.OpenSubMenu(jbPlayer.Controller, menu);
+    }
+}
