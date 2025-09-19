3737370de8aaf221b774739f3f5d137c9457a184
T3Marius <168598779+T3Marius@users.noreply.github.com>
2025-09-19 22:18:58 +0300

    feat: added IJBPlayer class and more functions

--- Jailbreak/src/JailbreakApi/JailbreakApi.cs
+++ Jailbreak/src/JailbreakApi/JailbreakApi.cs
@@ -1,9 +1,21 @@
-﻿using JailbreakApi;
+﻿using CounterStrikeSharp.API.Core;
+using Jailbreak.Config;
+using JailbreakApi;
+using Microsoft.Extensions.Localization;
+using CSTimer = CounterStrikeSharp.API.Modules.Timers.Timer;
+using static Jailbreak.Jailbreak;
 
 namespace Jailbreak;
 
 public class JailbreakApi : IJailbreakApi
 {
+    private readonly ConfigManager _configManager;
+
+    public JailbreakApi(ConfigManager configManager)
+    {
+        _configManager = configManager;
+    }
+
     public void RegisterDay(ISpecialDay day) => SpecialDayManagement.RegisterDay(day);
     public ISpecialDay? GetActiveDay() => SpecialDayManagement.GetActiveDay();
     public IReadOnlyList<ISpecialDay> GetAllDays() => SpecialDayManagement.GetDays();
@@ -14,5 +26,36 @@ public class JailbreakApi : IJailbreakApi
     public IReadOnlyList<ILastRequest> GetAllRequests() => LastRequestManagement.GetRequests();
     public void EndRequest() => LastRequestManagement.EndRequest();
 
-
+    public IJBPlayer? GetJBPlayer(CCSPlayerController controller)
+    {
+        return JBPlayerManagement.GetOrCreate(controller);
+    }
+    public IJBPlayer? GetWarden()
+    {
+        return JBPlayerManagement.GetWarden();
+    }
+    public CSTimer StartTimer(int seconds, Action<int> onTick, Action onFinished)
+    {
+        return Library.StartTimer(seconds, onTick, onFinished);
+    }
+    public void PrintToHtml(CCSPlayerController controller, string message, int duration)
+    {
+        controller.PrintToHtml(message, duration);
+    }
+    public T GetConfigValue<T>(string key, T defaultValue = default!)
+    {
+        return _configManager.GetConfigValue(key, defaultValue);
+    }
+    public LocalizedString GetLocalizer(string message, params object[] args)
+    {
+        return Instance.Localizer[message, args];
+    }
+    public void FreezePlayer(CCSPlayerController controller)
+    {
+        controller.Freeze();
+    }
+    public void UnfreezePlayer(CCSPlayerController controller)
+    {
+        controller.Unfreeze();
+    }
 }
